7f4fea2188a051ef90204407b29d31c8ebcdf537
Isaac Granillo <isaac.granillo@gmail.com>
2016-04-08 13:05:41 -0700

    added moment.js for dates, completed all socket connections and completed styling

--- server.js
+++ server.js
@@ -11,13 +11,34 @@ var server = app.listen(PORT, function(){
 
 var io = require('socket.io').listen(server);
 
+var users = [];
+
 io.sockets.on('connection', function(socket){
+
+	socket.on('newUser', function(userData){
+		users.push({
+			socketID: socket.id,
+			name: userData.userName
+		});
+		io.emit('updateUserList', users);
+		io.emit('updateMessageBoard', {
+			msg: ('<p>' + "<span id='new_user'>" + userData.userName + "</span>" + ' just entered the chat </p>')
+		});
+	});
 
 	socket.on('newMessage', function(userData){
 		io.emit('updateMessageBoard', userData);
 	});
 
 	socket.on('disconnect', function(){
-
+		for(index in users){
+			if(users[index].socketID == socket.id){
+				io.emit('updateMessageBoard', {
+					msg: ('<p>' + "<span id='past_user'>" + users[index].name + "</span>" + ' just left the chat </p>')
+				});
+				users.splice(index, 1);
+				io.emit('updateUserList', users);
+			};
+		};
 	});
 });
